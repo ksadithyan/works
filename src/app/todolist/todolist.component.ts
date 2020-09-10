55775b343ae7d0efcbb696b28263c1316411d8ee
ksadithyan <ksadithyan@gmail.com>
2020-09-10 18:05:41 +0530

    commit on 'master'

--- src/app/todolist/todolist.component.ts
+++ src/app/todolist/todolist.component.ts
@@ -9,6 +9,10 @@ export class TodolistComponent implements OnInit {
   todo:string
   list:string[]=[]
 
+  a:number
+  b:number
+  m:string[]=[]
+  
   constructor() { }
 
   ngOnInit(): void {
@@ -17,12 +21,25 @@ export class TodolistComponent implements OnInit {
   {
     if(this.todo != undefined && this.todo !="")
     {
-      if(this.list.length==0)
-    {this.list[0]= this.todo}
-    else
-    {this.list[this.list.length]=this.todo}
-    console.log(this.list)
+       if(this.list.length==0)
+         {this.list[0]= this.todo}
+       else
+         {this.list[this.list.length]=this.todo}
+         
+       console.log(this.list)
+       this.todo=""
     }
+  }
+  del(item:string)
+  {
+    this.a=this.list.indexOf(item)
+    this.list.splice(this.a,1)
+    console.log(this.list,"spliced")
+  }
+  mov(item:string)
+  { this.m.push(item)
+    this.b=this.list.indexOf(item)
+    this.list.splice(this.b,1) 
   }
 
 }
